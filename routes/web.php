7f507d3da9a65c3a80c37ae6abbffe64fb58e8a0
dionne emanuel <dion.prof.7@gmail.com>
2026-04-07 10:35:31 +0800

    First

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,38 @@
  
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegistrationController;
- 
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Public Pages
+|--------------------------------------------------------------------------
+*/
 Route::get('/', [RegistrationController::class, 'home'])->name('home');
- 
+
+/*
+|--------------------------------------------------------------------------
+| Registration (your original feature)
+|--------------------------------------------------------------------------
+*/
 Route::get('/register', [RegistrationController::class, 'showForm'])->name('register.form');
- 
 Route::post('/register', [RegistrationController::class, 'submitForm'])->name('register.submit');
+
+/*
+|--------------------------------------------------------------------------
+| Authentication (login system)
+|--------------------------------------------------------------------------
+*/
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
+
+/*
+|--------------------------------------------------------------------------
+| Protected Route
+|--------------------------------------------------------------------------
+*/
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth');
